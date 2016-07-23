ce07279f3b6eddd430a1a692f842b45937574ed7
gamester543 <gamesterrex@gmail.com>
2016-07-22 17:26:04 -0700

    Update Engine.js

--- scripts/Engine.js
+++ scripts/Engine.js
@@ -17,25 +17,7 @@ var shots = 1;
 var Shot;
 var Theme;
 
-function startGame() {
-    Area.start();
 
-    a = new ShipCore(480, 270);
-
-    for (var i = 0; i < 10; i++) {
-        spawners[i] = new Spawner(Math.floor((Math.random() * 100)), Math.floor((Math.random() * 100)));
-    }
-
-    //Shop section
-    buttons[0] = new button(10, 10, "Upgrade Ship",0);
-    buttons[1] = new button(10, 110, "Upgrade health", 5);
-    buttons[2] = new button(10, 210, "Buy Cannon", 10);
-    buttons[3] = new button(10, 260, "Buy Armor", 5);
-
-    chats = new Chat("Queen Davida: <To be added>", 5);
-    Shot = new sound("assets/shot.mp3");
-    Theme = new sound("assets/to_space.mp3");
-}
 
 function sound(src) {   //Sound elements
     this.sound = document.createElement("audio");
